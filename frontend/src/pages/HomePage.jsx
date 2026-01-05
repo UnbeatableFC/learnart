a201b428a5c53955e3533cc80bbd8cba52ce9ce9
Wisdom Mpamugo <wisdommpam99@gmail.com>
2026-01-05 13:55:39 +0100

    initial

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import HomeCourses from '../components/HomeCourses'
+import Testimonials from '../components/Testimonials'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Banner />
+        <HomeCourses />
+        <Testimonials />
+        <Footer />
+    </div>
+  )
+}
+
+export default HomePage
